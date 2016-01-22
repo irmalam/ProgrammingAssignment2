923db3774aef06b3477e717484dee61f4b939a93
irmalam <irmalam@uw.edu>
2016-01-21 23:10:33 -0800

    Week3 assignment2 - completed makeCacheMatrix and cacheSolve
    Commit to master

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,43 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
+## makeCacheMatrix creates a special “matrix”, which is really a list containing a function to
+## 1) set the value of the matrix
+## 2) get the value of the matrix
+## 3) set the inverse of the matrix
+## 4) get the inverse of the matrix
 makeCacheMatrix <- function(x = matrix()) {
-
+    m <- NULL
+    set <- function(y) {
+        x <<- y
+        m <<- NULL
+    }
+    get <- function() x
+    setinvM <- function(invM) m <<- invM
+    getinvM <- function() m
+    list(set = set, get = get,
+    setinvM = setinvM,
+    getinvM = getinvM)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve computes the inverse of the special “matrix” created with the makeCacheMatrix
+## function. It first checks to see if the inverse has already been computed. If so, it
+## retrieves the inverse from the cache, and skips the computation. Otherwise, it computes
+## the inverse of the matrix and sets the value of the inverse in the cache via the
+## setinvM() function.
 
+## For this assignment, assume that the matrix supplied is always invertible.
+## No need to check
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    m <- x$getinvM()
+	   data <- x$get()
+       
+       ## If the inverse has already been calculated (and the matrix has not changed),
+       ## then the cacheSolve should retrieve the inverse from the cache
+       if ((!is.null(m)) && (identical(x, data))) {
+           message("getting cached data")
+           return(m)
+       }
+       
+       m <- solve(data, ...)
+       x$setinvM(m)
+       m
 }
